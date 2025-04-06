7b10579e1f03116e906ad371f317ba3833d4c791
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-04-06 19:11:40 +0530

    bootstarpping wip
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+import { Test } from "@repo/ui/test";
+export default function Home() {
+  return (
+    <>
+      <div className="text-8xl font-bold text-center">Hello Turbo!!</div>
+      <Test />
+    </>
+  );
+}
