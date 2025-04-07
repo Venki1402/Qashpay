a286088e658062878c34f729601e32bd9714fc95
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-04-07 08:56:59 +0530

    wip
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

---
+++ packages/ui/src/test.tsx
@@ -0,0 +1,8 @@
+export const Test = () => {
+  return (
+    <div className="text-center">
+      <div className="h-10 w-10 bg-red-500 text-5xl"></div>
+      This component is to test if tailwing is working in packages/ui
+    </div>
+  );
+};
