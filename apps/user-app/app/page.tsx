373c3007541225977e97b83460582250ec9464c1
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-06-02 17:04:19 +0530

    reset😅 - starting afresh

--- apps/user-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Test } from "@repo/ui/test";
-export default function Home() {
-  return (
-    <>
-      <div className="text-8xl font-bold text-center">Hello Turbo!!</div>
-      <Test />
-    </>
-  );
-}
--- packages/ui/src/test.tsx
+++
@@ -1,8 +0,0 @@
-export const Test = () => {
-  return (
-    <div className="text-center">
-      <div className="h-10 w-10 bg-red-500 text-5xl"></div>
-      This component is to test if tailwing is working in packages/ui
-    </div>
-  );
-};
