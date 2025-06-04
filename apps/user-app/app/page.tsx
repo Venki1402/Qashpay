db31aa392624acb01ca577444b919f8ff6608181
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-06-04 08:16:30 +0530

    tailwind config

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@repo/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+      <Button />
+    </>
+  );
+};
+
+export default Home;
---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Button = () => {
+  return <div className="bg-black text-red-500 text-center">Button</div>;
+};
