63b2257a8e125c6c98acc18493bc4ef49714e823
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-06-04 07:47:01 +0530

    .

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@repo/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="text-3xl text-center text-red-500 font-mono">Home</div>
+    <>
+      <div className="text-3xl text-center text-red-500 font-mono">Home</div>
+      <Button appName="User App" >
+        Click me
+      </Button>
+    </>
   );
 };
 
